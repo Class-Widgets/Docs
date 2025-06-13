4ee4ad00cd8f94e082aa82e39c35fc30bdd44c51
lidongxun967 <1985409711@qq.com>
2025-06-13 09:54:15 +0800

    更新首页组件，添加状态管理以控制渲染，优化用户体验

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import type { ReactNode } from 'react';
+import Layout from '@theme/Layout';
+import styles from './index.module.css';
 
-export default function Home() {
+export default function Home(): ReactNode {
+  const [shouldRender, setShouldRender] = useState(true);
+
   useEffect(() => {
-    window.location.replace('https://classwidgets.rinlit.cn/');
+    if (process.env.NODE_ENV !== 'development') {
+      window.location.replace('https://classwidgets.rinlit.cn/');
+      setShouldRender(false);
+    }
   }, []);
-  return null;
+
+  if (!shouldRender) return null;
+
+  return (
+    <Layout>
+      <main className={styles.heroBanner}>
+        <div className={styles.contentContainer}>
+          <h1>提示</h1>
+          <h2>您当前正处于开发环境</h2>
+          <br />
+          <a href="/user_docs" className={styles.link}>用户文档</a>
+          <a href="/dev_docs" className={styles.link}>开发者文档</a>
+          <a href="/standards" className={styles.link}>社区规范</a>
+        </div>
+      </main>
+    </Layout>
+  );
 }
